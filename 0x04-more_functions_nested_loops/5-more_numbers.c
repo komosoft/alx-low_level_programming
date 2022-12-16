85dc399ecd4fa26eaf55e9cd924ce3aa60827104
komosoft <adebomianu@gmail.com>
2022-12-15 18:52:19 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,42 @@
+iamyinka
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+alx-low_level_programming/0x04-more_functions_nested_loops/5-more_numbers.c
+@iamyinka
+iamyinka Finish Task
+ 1 contributor
+Executable File  26 lines (23 sloc)  338 Bytes
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: Can only use _putchar 3 times
+ */
+void more_numbers(void)
+{
+	int c, i;
+
+	c = 0;
+	while (c < 10)
+	{
+		i = 0;
+		while (i < 15)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+			i++;
+		}
+		c++;
+		_putchar('\n');
+	}
+}
